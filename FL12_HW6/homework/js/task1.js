c57c93d557b9a7bda5d99e33b7d9280fa10cbdac
Taras Oleksiuk <oleksiuktaras@gmail.com>
2019-12-24 23:35:53 +0200

    fixed

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -3,6 +3,8 @@ let b = prompt('b');
 let c = prompt('c');
 let x1, x2;
 
+isValid = true;
+
 let two = 2;
 let four = 4;
 let minus1 = -1;
@@ -18,6 +20,7 @@ if (
     a === 0
 ) {
     console.log('Invalid input data');
+    isValid = false;
 }
 
 a = parseFloat(a);
@@ -26,18 +29,19 @@ c = parseFloat(c);
 
 if (isNaN(a) || isNaN(b) || isNaN(c)) {
     console.log('Invalid input data');
+    isValid = false;
 }
 let d = Math.pow(b, two) - four * a * c;
-if (d > 0) {
+if (isValid && (d > 0)) {
     x1 = (minus1 * b + Math.sqrt(Math.pow(b, two) - four * a * c)) / (two * a);
     x2 = (minus1 * b - Math.sqrt(Math.pow(b, two) - four * a * c)) / (two * a);
 
     console.log('x1 = ' + x1 + ' and x2 = ' + x2);
 }
-if (d === 0) {
+if (isValid && (d === 0)) {
     x1 = -b / (two * a);
     console.log('x = ' + x1);
 }
-if (d < 0) {
-    console.log('no solution (when Discriminant < 0)');
+if (isValid && (d < 0)) {
+    console.log('no solution');
 }
