5a0a542d7d58df0f6cd622b872595760b56bc6f3
Taras Oleksiuk <oleksiuktaras@gmail.com>
2019-12-24 22:31:47 +0200

    fixed bugs

--- FL12_HW6/homework/js/task1.js
+++ FL12_HW6/homework/js/task1.js
@@ -1,1 +1,38 @@
-// Your code goes here
+let a = prompt('a');
+let b = prompt('b');
+let c = prompt('c');
+let x1, x2;
+
+if (
+    a === '' ||
+    b === '' ||
+    c === '' ||
+    a === undefined ||
+    b === undefined ||
+    c === undefined ||
+    a === 0
+) {
+    console.log('Invalid input data');
+}
+
+a = parseFloat(a);
+b = parseFloat(b);
+c = parseFloat(c);
+
+if (isNaN(a) || isNaN(b) || isNaN(c)) {
+    console.log('Invalid input data');
+}
+let d = Math.pow(b, 2) - 4 * a * c;
+if (d > 0) {
+    x1 = (-1 * b + Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
+    x2 = (-1 * b - Math.sqrt(Math.pow(b, 2) - 4 * a * c)) / (2 * a);
+
+    console.log('x1 = ' + x1 + ' and x2 = ' + x2);
+}
+if (d === 0) {
+    x1 = -b / (2 * a);
+    console.log('x = ' + x1);
+}
+if (d < 0) {
+    console.log('no solution (when Discriminant < 0)');
+}
--- FL12_HW6/homework/js/task2.js
+++ FL12_HW6/homework/js/task2.js
@@ -1,1 +1,47 @@
-// Your code goes here
+
+let a = prompt('a');
+let b = prompt('b');
+let c = prompt('c');
+let isValid = true;
+
+if (
+  a === '' ||
+  b === '' ||
+  c === '' ||
+  a === undefined ||
+  b === undefined ||
+  c === undefined
+) {
+  alert('input values should be ONLY numbers');
+}
+
+a = parseInt(a);
+b = parseInt(b);
+c = parseInt(c);
+
+if (isNaN(a) || isNaN(b) || isNaN(c)) {
+  alert('input values should be ONLY numbers');
+  isValid = false;
+}
+
+if (a <= 0 || b <= 0 || c <= 0) {
+  alert('A triangle must have 3 sides with a positive definite length');
+  isValid = false;
+}
+
+if (a + b <= c || a + c <= b || b + c <= a) {
+  alert('Triangle doesn’t exist');
+  isValid = false;
+}
+
+if (isValid && (a === b && a === c && b === c)) {
+  alert('Equilateral triangle');
+}
+
+if (isValid && (b === c || a === b || c === a)) {
+  alert('Isosceles triangle');
+}
+
+if (isValid && (a !== b && a !== c && b !== c)) {
+  alert('Scalene triangle');
+}
